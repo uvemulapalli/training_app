c8b59762a26e45f2a99fa149140e731723f7befa
satyac01 <babi2k5@gmail.com>
2023-03-28 22:37:36 +0530

    aaa

--- TrainingApp.py
+++ TrainingApp.py
@@ -59,7 +59,7 @@ def PersistTrainingSetForInstruments():
                 logger.info("training data doesn't exists in Redis")
             xTrain,yTrain,dydxTrain = generateTrainingData(instrumentId,spotPrice,strikePrice, volatality, expiryInDays)
             model_training_data = np.concatenate((xTrain, yTrain, dydxTrain), axis=1)
-            df = pd.DataFrame(model_training_data, columns=['price', 'spot', 'differential'])
+            df = pd.DataFrame(model_training_data, columns=['spot', 'price', 'differential'])
             trainingSetDict= df.to_dict(orient="records")
             redis_instrument_con.set(instrumentId,json.dumps(trainingSetDict))
             print("record added",instrumentId)
@@ -97,7 +97,7 @@ def GetTrainingSetForGivenInstruments():
                 xTrain, yTrain, dydxTrain = generateTrainingData(instrumentId, spotPrice, strikePrice, volatality,
                                                            expiryInYears)
                 model_training_data = np.concatenate((xTrain, yTrain, dydxTrain), axis=1)
-                df = pd.DataFrame(model_training_data, columns=['price', 'spot', 'differential'])
+                df = pd.DataFrame(model_training_data, columns=['spot', 'price', 'differential'])
                 trainingSetDict = df.to_dict(orient="records")
                 try:
                     redis_instrument_con.__setitem__(instrumentId, json.dumps(trainingSetDict))
@@ -114,7 +114,7 @@ def GetTrainingSetForGivenInstruments():
             xTrain, yTrain, dydxTrain = generateTrainingData(instrumentId, spotPrice, strikePrice, volatality,
                                                        expiryInYears)
             model_training_data = np.concatenate((xTrain, yTrain, dydxTrain), axis=1)
-            df = pd.DataFrame(model_training_data, columns=['price', 'spot', 'differential'])
+            df = pd.DataFrame(model_training_data, columns=['spot', 'price', 'differential'])
             trainingSetDict = df.to_dict(orient="records")
             try:
                 redis_instrument_con.__setitem__(instrumentId, json.dumps(trainingSetDict))
@@ -172,13 +172,17 @@ def connectToMongo(db=0):
 
 def generateTrainingData(instrumentId,spotPrice,strikePrice,volatality,expiryInYears):
     generator = BlackScholes()
-    generator.__init__(spot=((spotPrice)/100), K=((strikePrice)/100), vol=volatality, T2=(1 + expiryInYears))
+    spotPrice = spotPrice / 100
+    strikePrice = strikePrice / 100
+    generator.__init__(spot=(spotPrice), K=(strikePrice), vol=volatality, T2=(1 + expiryInYears))
     xTrain, yTrain, dydxTrain = generator.trainingSet(size)
     return xTrain, yTrain, dydxTrain
 
 
 def generateTestData(instrumentId,spotPrice,strikePrice,volatality,expiryInYears):
     generator = BlackScholes()
+    spotPrice = spotPrice/100
+    strikePrice = strikePrice/100
     generator.__init__(spot=(spotPrice), K=(strikePrice), vol=volatality, T2=(1 + expiryInYears))
     lowerBound = ( spotPrice - (spotPrice * 0.05) )
     upperBound = ( spotPrice + (spotPrice * 0.05) )
